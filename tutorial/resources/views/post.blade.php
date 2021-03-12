a765304540dbbd49936a9a26e466af2786a6cb14
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2021-03-12 10:22:00 +0100

    Startet op på routes

--- tutorial/resources/views/post.blade.php
+++ tutorial/resources/views/post.blade.php
@@ -8,6 +8,8 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>{{ $name }}</h1>
+<h1>My blog post</h1>
+
+<p>{{ $post }}</p>
 </body>
 </html>
--- tutorial/routes/web.php
+++ tutorial/routes/web.php
@@ -16,6 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// vi bruger test viewet og viser hvad brugeren har skrevet i url, desuden har vi brugt inline som svarer til Lambda i java.
 Route::get('/test', function () {
-    return view('test');
+    return view('test', [
+        'name' => request('name')
+    ]);
+});
+// ny route der tager et wildcard {post} og henter fra dummy data
+Route::get('/posts/{post}', function ($post) {
+    $posts = [
+        'my-first-post' => 'Hello this is my first blog post!',
+        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+    ];
+    // i tilfælde af manglende post
+    if (! array_key_exists($post, $posts)){
+        abort(404, 'Sorry that post was not found');
+    }
+    return view('post', [
+        'post' => $posts[$post]
+    ]);
 });
