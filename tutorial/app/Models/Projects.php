a4fb6f0370ee43ae03b784a6e6ff090cdf3c8d5c
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2021-05-01 13:51:17 +0200

    Nr 29 complete

--- tutorial/app/Models/Projects.php
+++ tutorial/app/Models/Projects.php
@@ -5,17 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
+class Projects extends Model
 {
     use HasFactory;
-    protected $guarded = [];
-
-    public function path(){
-        return route('articles.show', $this);
-    }
 
     public function user(){
         return $this->belongsTo(User::class);
     }
-
 }
