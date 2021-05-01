3a2b5b19a21667cbc76e88c164969b611c0a4d01
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2021-05-01 17:11:39 +0200

    Nr 44 complete

--- tutorial/app/Http/Controllers/ContactController.php
+++ tutorial/app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMe;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
@@ -13,10 +14,8 @@ class ContactController extends Controller
     public function store(){
         request()->validate(['email' => 'required|email']);
 
-        Mail::raw('It works!', function($message){
-            $message->to(request('email'))
-                ->subject('Hello there');
-        });
+        Mail::to(request('email'))
+            ->send(new ContactMe('shirts'));
 
         return redirect('/contact')
             ->with('message', 'Email sent!');
--- tutorial/resources/views/contact.blade.php
+++ tutorial/resources/views/contact.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta
         http-equiv="Content-Type"
-        content="text/html;charset=UTG-8"
+        content="text/html;charset=UTF-8"
     >
 
     <title>Mail Lesson</title>
