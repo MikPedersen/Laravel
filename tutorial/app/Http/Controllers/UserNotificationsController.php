e8bd8a899e79e19868dc010121e9d6a31d198ed3
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2021-05-02 14:52:43 +0200

    Nr 48 complete

---
+++ tutorial/app/Http/Controllers/UserNotificationsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserNotificationsController extends Controller
+{
+    public function show(){
+        $notifications = auth()->user()->unreadNotifications;
+
+//        foreach ($notifications as $notification){
+//            $notification->markAsRead();
+//        }
+
+        $notifications->markAsRead();
+
+        return view('notifications.show', [
+            'notifications' => $notifications
+        ]);
+
+//        return view('notifications.show', [
+//            'notifications' => tap(auth()->user()->unreadNotifications->markAsRead())
+//        ]);
+    }
+
+
+}
